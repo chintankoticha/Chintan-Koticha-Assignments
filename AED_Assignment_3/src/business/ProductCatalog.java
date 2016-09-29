e4ebf494472139536c11472da46862fa749367e1
chintan koticha <koticha.c@husky.neu.edu>
2016-09-29 01:32:33 -0400

    Adding assignment-3 commit with basic UI

---
+++ AED_Assignment_3/src/business/ProductCatalog.java
@@ -0,0 +1,87 @@
+package business;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Chintan
+ */
+public class ProductCatalog {
+    private String productName;
+    private String modelNumber;
+    private String vendorName;
+    private String productDescriptionStatement;
+    float basePrice;
+    float ceilProce;
+    float floorPrice;
+    private String features;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getProductDescriptionStatement() {
+        return productDescriptionStatement;
+    }
+
+    public void setProductDescriptionStatement(String productDescriptionStatement) {
+        this.productDescriptionStatement = productDescriptionStatement;
+    }
+
+    public float getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(float basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public float getCeilProce() {
+        return ceilProce;
+    }
+
+    public void setCeilProce(float ceilProce) {
+        this.ceilProce = ceilProce;
+    }
+
+    public float getFloorPrice() {
+        return floorPrice;
+    }
+
+    public void setFloorPrice(float floorPrice) {
+        this.floorPrice = floorPrice;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+    
+}
---
+++ AED_Assignment_3/src/business/ProductCatalogDirectory.java
@@ -0,0 +1,46 @@
+package business;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author Chintan
+ */
+public class ProductCatalogDirectory {
+    public ArrayList<ProductCatalog> productsList;
+
+    public ProductCatalogDirectory() {
+       this.productsList = new ArrayList<ProductCatalog>();
+    }
+
+    public ArrayList<ProductCatalog> getAccountList() {
+        return productsList;
+    }
+
+    public void setAccountList(ArrayList<ProductCatalog> productsList) {
+        this.productsList = productsList;
+    } 
+    
+    public ProductCatalog addProductCatalog(){
+        ProductCatalog productcatalog = new ProductCatalog();
+        productsList.add(productcatalog);
+        return productcatalog;
+    }
+    
+    public void delete(ProductCatalog productcatalog){
+        productsList.remove(productcatalog);
+    }
+    
+    /*public ProductCatalog searchProducts(String accountNumber){
+        for (Account account : accountList) {
+            if(account.getAccountNumber().equals(accountNumber)){
+                return account;
+            }
+        }
+        return null;
+    }  */
+}
